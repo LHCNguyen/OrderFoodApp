9de604dc86f84684c8508a86efe0e083c3d16e3a
LHCNguyen <lhcnguyen03@gmail.com>
2024-10-16 19:49:41 +0700

    Project mobile

---
+++ OrderFoodApp/app/src/main/java/com/example/orderfoodapp/DangKyActivity.java
@@ -0,0 +1,28 @@
+package com.example.orderfoodapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.orderfoodapp.Database.CreateDatabase;
+
+public class DangKyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.layout_dangky);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        CreateDatabase createDatabase = new CreateDatabase(this);
+        createDatabase.open();
+    }
+}
